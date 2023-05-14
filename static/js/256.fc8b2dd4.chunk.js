e9672de2a2e933bfb20ff9799a65e2ee1c4eedc1
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-05-14 11:35:08 +0000

    Deploying to gh-pages from @ Anastasiia-Ablieieva/goit-react-hw-05-movies@aca5af14dfa72fa5a43b92d6b924550c54e01f1b 🚀

--- static/js/256.fc8b2dd4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t),n.d(t,{NotFound:function(){return a}});var r=n(87),o=n(184),a=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:" Page not found..."}),(0,o.jsx)(r.OL,{to:"/",children:" Back to Home Page "})]})};t.default=a}}]);
-//# sourceMappingURL=256.fc8b2dd4.chunk.js.map
